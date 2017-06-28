5e46001f34f161249c49b4d0a7a0c909fd555dda
David Carley <dacarley@gmail.com>
2017-06-27 20:16:50 -0700

    Code formatting, enabled providesModule on client

---
+++ client/src/components/WaterLevels.js
@@ -0,0 +1,13 @@
+// @providesModule AQ-WaterLevels
+
+import React, { Component } from "react";
+
+export default class WaterLevels extends Component {
+    render() {
+        return (
+            <p>
+                Water levels go here.
+            </p>
+            );
+    }
+}
